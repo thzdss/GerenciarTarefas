c87a1a10de98d91549d4145ace095bd655b01435
thzdss <thzdss440@gmail.com>
2025-06-05 18:10:05 -0300

    Criação e configuração das Classes Entidades Tarefa e Usuario

--- src/main/java/com/gerenciadortarefas/GerenciarTarefas/Model/Tarefa.java
+++ src/main/java/com/gerenciadortarefas/GerenciarTarefas/Model/Tarefa.java
@@ -1,4 +1,37 @@
 package com.gerenciadortarefas.GerenciarTarefas.Model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "tarefa")
+@Component
 public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titulo;
+    private String descricao;
+    @Enumerated(EnumType.STRING)
+    private Prioridade prioridade;
+    private LocalDate dataLimite;
+    private boolean concluida = false;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id")
+    @JsonBackReference
+    private Usuario usuario;
+
+
+    public enum Prioridade {
+        ALTA, MEDIA, BAIXA
+    }
+
+
 }
--- src/main/java/com/gerenciadortarefas/GerenciarTarefas/Model/Usuario.java
+++ src/main/java/com/gerenciadortarefas/GerenciarTarefas/Model/Usuario.java
@@ -1,4 +1,31 @@
 package com.gerenciadortarefas.GerenciarTarefas.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "usuario") // Renomeado para 'usuario'
+@Component
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+
+
+    @OneToMany(mappedBy = "usuario", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, fetch = FetchType.LAZY)
+    private List<Tarefa> tarefas = new ArrayList<>();
+
+
+    public void adicionarTarefa(Tarefa tarefa) {
+        tarefa.setUsuario(this);
+        this.tarefas.add(tarefa);
+    }
 }
