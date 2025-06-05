830dfd84960bce697fd94921717abee0b0355932
thzdss <thzdss440@gmail.com>
2025-06-05 17:27:32 -0300

    Criação do Projeto Java

---
+++ src/main/java/com/gerenciadortarefas/GerenciarTarefas/GerenciarTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciadortarefas.GerenciarTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarTarefasApplication.class, args);
+	}
+
+}
