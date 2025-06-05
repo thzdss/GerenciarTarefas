fc26d5e6a2dede27da70964c21eac2e084ae39ab
thzdss <thzdss440@gmail.com>
2025-06-05 18:11:11 -0300

    Atualização das Interfaces Repository

--- src/main/java/com/gerenciadortarefas/GerenciarTarefas/Repository/TarefaRepository.java
+++ src/main/java/com/gerenciadortarefas/GerenciarTarefas/Repository/TarefaRepository.java
@@ -1,4 +1,12 @@
 package com.gerenciadortarefas.GerenciarTarefas.Repository;
 
-public interface TarefaRepository {
+import com.gerenciadortarefas.GerenciarTarefas.Model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Integer> {
+    List<Tarefa> findByUsuarioId(int usuarioId);
 }
--- src/main/java/com/gerenciadortarefas/GerenciarTarefas/Repository/UsuarioRepository.java
+++ src/main/java/com/gerenciadortarefas/GerenciarTarefas/Repository/UsuarioRepository.java
@@ -1,4 +1,11 @@
 package com.gerenciadortarefas.GerenciarTarefas.Repository;
 
-public interface UsuarioRepository {
+
+import com.gerenciadortarefas.GerenciarTarefas.Model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Usuario findByEmail(String email);
 }
