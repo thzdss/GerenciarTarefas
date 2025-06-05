e73d340efd6b6a4c014b5cabf91284d8e1f2baa8
thzdss <thzdss440@gmail.com>
2025-06-05 18:12:02 -0300

    Criação dos Métodos das Classes Service

--- src/main/java/com/gerenciadortarefas/GerenciarTarefas/Service/TarefaService.java
+++ src/main/java/com/gerenciadortarefas/GerenciarTarefas/Service/TarefaService.java
@@ -1,4 +1,36 @@
 package com.gerenciadortarefas.GerenciarTarefas.Service;
 
+import com.gerenciadortarefas.GerenciarTarefas.Model.Tarefa;
+import com.gerenciadortarefas.GerenciarTarefas.Repository.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TarefaService {
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    public List<Tarefa> listarTodas() {
+        return tarefaRepository.findAll();
+    }
+
+    public Tarefa salvar(Tarefa tarefa) {
+        return tarefaRepository.save(tarefa);
+    }
+
+    public void excluir(int id) {
+        tarefaRepository.deleteById(id);
+    }
+
+    public Tarefa buscarPorId(int id) {
+        return tarefaRepository.findById(id).orElse(null);
+    }
+
+
+    public List<Tarefa> listarPorUsuario(int usuarioId) {
+        return tarefaRepository.findByUsuarioId(usuarioId);
+    }
 }
--- src/main/java/com/gerenciadortarefas/GerenciarTarefas/Service/UsuarioService.java
+++ src/main/java/com/gerenciadortarefas/GerenciarTarefas/Service/UsuarioService.java
@@ -1,4 +1,36 @@
 package com.gerenciadortarefas.GerenciarTarefas.Service;
 
+import com.gerenciadortarefas.GerenciarTarefas.Model.Usuario;
+import com.gerenciadortarefas.GerenciarTarefas.Repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario buscarPorId(int id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void excluir(int id) {
+        usuarioRepository.deleteById(id);
+    }
+
+
+    public Usuario buscarPorEmail(String email) {
+        return usuarioRepository.findByEmail(email);
+    }
 }
