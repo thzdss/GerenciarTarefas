29024d116d4d30d286b7f0d12586e1adcbaa30e3
thzdss <thzdss440@gmail.com>
2025-06-05 18:06:02 -0300

    Atualização SiteController

--- src/main/java/com/gerenciadortarefas/GerenciarTarefas/Controller/SiteController.java
+++ src/main/java/com/gerenciadortarefas/GerenciarTarefas/Controller/SiteController.java
@@ -1,4 +1,13 @@
 package com.gerenciadortarefas.GerenciarTarefas.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SiteController {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "index";
+    }
 }
